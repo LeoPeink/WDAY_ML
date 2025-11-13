9b164af1f2eb7b87190be12660a7558076a33f0f
LeoPeink <leo.peinkhofer@gmail.com>
2025-11-13 16:23:53 +0100

    Added lines to visualize the sine function data points using a scatter plot.

--- notebooks/Lab_2.SOL.py
+++ notebooks/Lab_2.SOL.py
@@ -6,7 +6,34 @@
 
 
 import numpy as np
-import notebooks.LP_LIB as lp
+import LP_LIB as lp
 import matplotlib.pyplot as plt
 
+#STEP 1:
+#generate uniform, random X array
+#generate y = sin(pi*x)
 
+    #(1) ASAIW:
+    #generate gaussian, random noise array as eps
+    #re-generate y = sin(pi*x)+eps
+
+n = 500  #number of points       (observations)
+dim = 1 #number of dimensions   (features)
+l = 0   #lower bound for x
+u = 100 #upper bound for x
+#X = np.array(np.random.uniform(l,u,n))
+X = np.linspace(-100,100)
+print(X) 
+y = np.sin(np.pi*X)
+print(y)  
+
+plt.scatter(X,y)
+plt.show()
+
+
+"""
+X,y = lp.linDataGen(50,1,0,1,None,0.1)
+print(X)
+plt.scatter(X,y)
+plt.show()
+"""
